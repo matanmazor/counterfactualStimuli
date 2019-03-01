cb06b101dc086fe70545590ff5367c0597b94b38
Matan Mazor <mmazor@fil.ion.ucl.ac.uk>
2019-03-01 13:37:40 +0000

    added leading trials

--- createContrastSchedule.m
+++ createContrastSchedule.m
@@ -2,9 +2,9 @@ function [ contrast_schedule ] = createContrastSchedule( N_trials, step_std )
 
 
 %1. do random walk
-x = cumsum(normrnd(0,step_std, N_trials,1));
-%2. mean center
-contrast_schedule = x-mean(x);
+x = cumsum(normrnd(0,step_std, N_trials-10,1));
+%2. mean center and add leading trials
+contrast_schedule = [linspace(-1.5,-2.5)'; x-mean(x)];
 
 end
 
