37add6800218f19fe2440ed7892ab0e1c7a7cfde
Matan Mazor <mmazor@fil.ion.ucl.ac.uk>
2019-03-04 12:01:02 +0000

    added length specification to linspace

--- createContrastSchedule.m
+++ createContrastSchedule.m
@@ -4,7 +4,7 @@ function [ contrast_schedule ] = createContrastSchedule( N_trials, step_std )
 %1. do random walk
 x = cumsum(normrnd(0,step_std, N_trials-10,1));
 %2. mean center and add leading trials
-contrast_schedule = [linspace(-1.5,-2.5)'; x-mean(x)]';
+contrast_schedule = [linspace(-1.5,-2.5,10)'; x-mean(x)];
 
 end
 
